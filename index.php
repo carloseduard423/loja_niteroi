18b4d10c66bb111dadf52f58a98b1ff322761613
carloseduard423 <carlos.eduardo040902@gmail.com>
2020-08-06 20:15:48 -0300

    página inicial incompleta

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bem vindo à nossa loja</title>
+</head>
+<body>
+<?php
+	$nome = "Carlos";
+?>
+	<nav>
+		<ul>
+			<li>Cadastre-se</li>
+			<li>Área Restrita</li>
+		</ul>
+	</nav>
+
+	Categorias
+	Produtos
+
+</body>
+</html>
